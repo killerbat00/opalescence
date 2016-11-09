2fca9eb49a9294f8a5031f85f68c55396a153bd9
brian houston morrow <bhm@brianmorrow.net>
2016-11-09 00:00:18 -0600

    no more hardcoded paths!

--- default.py
+++ default.py
@@ -1,18 +1,21 @@
+import os
+
 LOGGING = False
 DEBUG = True
 
-root = "Y:\Personal\Development\opalescence\\btlib\\tests\\data"
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+root = os.path.join(PROJECT_DIR, 'btlib', 'tests', 'data')
 
-TEST_FILE = "\\".join([root, "test_torrents", "torrent_from_dir.torrent"])
-TEST_OUTPUT_FILE = "\\".join([root, "test_torrents", "op_test_output.torrent"])
+TEST_FILE = os.path.join(root, "test_torrents", "torrent_from_dir.torrent")
+TEST_OUTPUT_FILE = os.path.join(root, "test_torrents", "op_test_output.torrent")
 
-TEST_TORRENT_DIR = "\\".join([root, "test_torrent_dir"])
-TEST_TORRENT_DIR_OUTPUT = "\\".join([root, "test_torrents", "op_test_torrent_dir.torrent"])
+TEST_TORRENT_DIR = os.path.join(root, "test_torrent_dir")
+TEST_TORRENT_DIR_OUTPUT = os.path.join(root, "test_torrents", "op_test_torrent_dir.torrent")
 
-TEST_EXTERNAL_FILE = "\\".join([root, "test_torrents", "qtbittorrent.torrent"])
-TEST_EXTERNAL_OUTPUT = "\\".join([root, "test_torrents", "op_qtbittorrent.torrent"])
+TEST_EXTERNAL_FILE = os.path.join(root, "test_torrents", "qtbittorrent.torrent")
+TEST_EXTERNAL_OUTPUT = os.path.join(root, "test_torrents", "op_qtbittorrent.torrent")
 
-STAR_TREK = "\\".join([root, "test_torrents", "star_trek.torrent"])
+STAR_TREK = os.path.join(root, "test_torrents", "star_trek.torrent")
 
 NAME = "opalescence"
 VERSION = "0.2"
