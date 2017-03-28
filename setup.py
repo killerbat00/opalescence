b03a88ca402541b4596237f356f0597daf86a70e
brian houston morrow <bhm@brianmorrow.net>
2017-03-27 23:28:01 -0500

    create basic setup file

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    "requests"
+]
+
+setup(
+    name="opalescence",
+    version="0.1.1",
+    description="Torrent client offering basic functionality.",
+    long_description=readme,
+    author="brian houston morrow",
+    author_email="bhm@brianmorrow.net",
+    url="https://github.com/killerbat00/opalescence",
+    packages=[
+        "opalescence"
+    ],
+    package_dir={"opalescence": "opalescence"},
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    keywords="torrent",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.6"
+    ],
+    test_suite="tests",
+)
