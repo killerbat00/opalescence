eb252bc701e25b20fb13adff1b86afdcb90200c5
brian houston morrow <bhm@brianmorrow.net>
2017-04-03 02:48:33 -0500

    peer handshake implemented.

--- opalescence/btlib/peer.py
+++ opalescence/btlib/peer.py
@@ -39,80 +39,36 @@ class Peer:
         self.am_interested = False
         self.peer_choking = True
         self.peer_interested = False
-        self.future = asyncio.ensure_future(self._start())
+        # self.future = asyncio.ensure_future(self._start())
 
     def __str__(self):
         return "{ip}:{port}".format(ip=self.ip, port=self.port)
 
     async def _start(self):
         while True:
-            logger.debug("Initiating handshake with peer {peer}".format(peer=self))
-
             try:
                 self.reader, self.writer = await asyncio.open_connection(host=self.ip, port=self.port)
-                logger.debug("Opened connection with peer: {ip:port}".format(ip=self.ip, port=self.port))
+                logger.debug(f"Opened connection with peer: {self.ip}:{self.port}")
 
                 buffer = await self._handshake()
-                print(buffer)
             except OSError:
                 logger.debug("Unable to open connection to {peer}".format(peer=self))
-                return
+                raise
 
     async def _handshake(self):
-        msg = struct.pack('>B19s8x20s20s', 19, b'BitTorrent protocol', '', self.info_hash, self.peer_id)
-        # construct message
+        logger.debug("Initiating handshake with peer {peer}".format(peer=self))
+        msg = struct.pack('>B19s8x20s20s', 19, b'BitTorrent protocol', self.info_hash, self.peer_id)
         self.writer.write(msg)
         await self.writer.drain()
 
         buf = b''
+        logger.debug("receiving handshake data")
         while len(buf) < self._handshake_len:
-            buf = await self.reader.read()
-            print(buf)
-            self._parse_msg(buf, msg)
-            # decode response
+            buf = await self.reader.read(10 * 1024)
 
-            #        except:
-            #            logger.debug("Error writing to {peer}".format(peer=self))
-            #            return
-            #        try:
-            #            chunks = await reader.read()
-            #            if not chunks:
-            #                logger.debug("No data received")
-            #                return
-            #        except:
-            #            logger.debug("Error reading from {peer}".format(peer=self))
-            #            return
-            #        msg = "{pstrlen}{pstr}{reserved}{info_hash}{peer_id}".format(pstrlen=self._pstr_len_bytes, pstr=PSTR,
-            #                                                                     reserved=self._reserved, info_hash=self.info_hash,
-            #                                                                     peer_id=self.peer_id).encode("ISO-8859-1")
-            #        message = "%s%s%s%s%s" % (chr(19), "BitTorrent protocol", 8 * chr(0),
-            #                                  self.info_hash, self.peer_id)
-            #        message = message.encode("ISO-8859-1")
-            #
-            #        loop = asyncio.get_event_loop()
-            #
-            #        print(chunks)
-            #        self._parse_msg(chunks, msg)
-            #        logger.debug("[*] Received message {message}".format(message=chunks.decode("ISO-8859-1")))
+        resp = self._parse_msg(buf[:self._handshake_len])
+        logger.debug(f"decoded message from peer {resp}")
+        return resp
 
-    def _parse_msg(self, message: bytes, orig: bytes):
-        parts = struct.unpack('>B19s8x20s20s', message[:68])
-        print(struct.unpack("B" * len(message), message))
-        a = message[:1]
-        b = message[1:69]
-        #        print(orig)
-        #        print(a)
-        #        print(b)
-
-        #        message = BytesIO(message)
-        #        print(binascii.hexlify(message))
-        #        msg_len = int.from_bytes(message.read(1), byteorder='big')
-        #        other_pstrlen = int.from_bytes(message.read(1), byteorder='big')
-        #        other_pstr = message.read(other_pstrlen)
-        #        other_reserved = message.read(8)
-        #        other_infohash = message.read(20)
-        #        other_peerid = message.read(20)
-        #        a = message.read()
-        #        a = other_pstr.decode("ISO-8859-1")
-        print("hmm")
-        return
+    def _parse_msg(self, message: bytes) -> tuple:
+        return struct.unpack('>B19s8x20s20s', message)
--- tests/test_peer.py
+++ tests/test_peer.py
@@ -3,12 +3,14 @@
 """
 Contains tests for the peer functionality of btlib
 """
+import asyncio
 import os
 from unittest import TestCase
 
 from requests import get
 
-from tests.context import torrent, tracker
+from tests.context import torrent, tracker, peer
+from tests.utils import async_run
 
 
 class TestPeer(TestCase):
@@ -17,6 +19,7 @@ class TestPeer(TestCase):
     """
     external_torrent_path = os.path.abspath(os.path.dirname(__file__))
     torrent_url = "http://releases.ubuntu.com/16.04/ubuntu-16.04.2-desktop-amd64.iso.torrent"
+    tracker = None
 
     @classmethod
     def setUpClass(cls):
@@ -33,7 +36,26 @@ class TestPeer(TestCase):
         cls.torrent = torrent.Torrent.from_file(cls.external_torrent_path)
         cls.tracker = tracker.Tracker(cls.torrent)
 
+    @classmethod
+    def tearDownClass(cls):
+        """
+        Closes the tracker's http client session
+        """
+        cls.tracker.close()
+
     def test_basic_comm(self):
         """
         """
-        self.fail()
+        resp = async_run(self.tracker.announce())
+        peers = resp.peers
+
+        for p in peers:
+            pp = peer.Peer(p[0], p[1], self.torrent.info_hash, self.tracker.peer_id)
+            try:
+                async_run(asyncio.wait_for(pp._start(), 5.0))
+            except asyncio.futures.TimeoutError:
+                print("TimeoutError")
+                continue
+            except OSError:
+                print("OSError")
+                continue
