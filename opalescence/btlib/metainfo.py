d47b3f28b14cb8617f473660c3ae4532f81ef637
brian houston morrow <bhm@brianmorrow.net>
2020-09-07 23:28:47 -0500

    Use string keys and improve support for multiple file torrents and announce-list

--- opalescence/btlib/metainfo.py
+++ opalescence/btlib/metainfo.py
@@ -10,6 +10,7 @@ import hashlib
 import logging
 import os
 from collections import OrderedDict
+from pathlib import Path
 from typing import NamedTuple, List, Optional
 
 from btproto import Decoder, Encoder, DecodeError, EncodeError
@@ -27,8 +28,12 @@ class FileItem(NamedTuple):
     """
     An individual file within a torrent.
     """
-    path: str
+    path: Path
     size: int
+
+
+def _get_and_decode(d: dict, k: str):
+    return d.get(k, b'').decode("UTF-8")
 
 
 def _pc(piece_string: bytes, *, length: int = 20, start: int = 0):
@@ -54,26 +59,24 @@ def _validate_torrent_dict(decoded_dict: OrderedDict) -> bool:
     :return:             True if valid
     :raises:             CreationError
     """
-    min_req_keys: List[str] = ["info", "announce"]
-    min_info_req_keys: List[str] = ["piece length", "pieces", "name"]
+    min_info_req_keys: List[str] = ["piece length", "pieces"]
     min_files_req_keys: List[str] = ["length", "path"]
 
     logger.debug(
         "Validating torrent metainfo dictionary {d}".format(d=decoded_dict))
 
     dict_keys: List = list(decoded_dict.keys())
-
     if not dict_keys:
         logger.error("No valid keys in dictionary.")
         raise CreationError
 
-    for key in min_req_keys:
-        if key not in dict_keys:
-            logger.error(f"Required key not found: {key}")
-            raise CreationError
+    if "info" not in dict_keys or \
+        "announce" not in dict_keys or \
+        "announce-list" not in dict_keys:
+        logger.error(f"Required key not found.")
+        raise CreationError
 
-    info_keys: List = list(decoded_dict["info"].keys())
-
+    info_keys: list = list(decoded_dict["info"].keys())
     if not info_keys:
         logger.error("No valid keys in info dictionary.")
         raise CreationError
@@ -103,7 +106,7 @@ def _validate_torrent_dict(decoded_dict: OrderedDict) -> bool:
                     raise CreationError
     else:
         if "length" not in info_keys:
-            logger.error("Required key not found: b'length'")
+            logger.error("Required key not found: 'length'")
             raise CreationError
 
     # we made it!
@@ -117,11 +120,12 @@ class MetaInfoFile:
     Unsupported metainfo keys:
         encoding
     """
+
     def __init__(self):
         self.files: List[FileItem] = []
         self.meta_info: Optional[OrderedDict] = None
         self.info_hash: bytes = b''
-        self.pieces: List[bytes] = []
+        self.piece_hashes: List[bytes] = []
 
     def __str__(self):
         return f"{self.name} <{self.info_hash}>"
@@ -158,8 +162,7 @@ class MetaInfoFile:
             info: bytes = Encoder(torrent.meta_info["info"]).encode()
             torrent.info_hash = hashlib.sha1(info).digest()
             torrent._gather_files()
-            torrent.pieces = list(_pc(torrent.meta_info["info"]["pieces"]))
-
+            torrent.piece_hashes = list(_pc(torrent.meta_info["info"]["pieces"]))
         except (EncodeError, DecodeError, IOError, Exception) as e:
             logger.error(f"Encountered error creating MetaInfoFile.")
             logger.info(e, exc_info=True)
@@ -215,29 +218,10 @@ class MetaInfoFile:
                 logger.info(ee, exc_info=True)
                 raise CreationError from ee
 
-    def _gather_files(self):
+    def _gather_files(self) -> None:
         """
         Gathers the files located in the torrent
-        """
-        # TODO: filepaths are a list containing string elements
-        if self.multi_file:
-            for f in self.meta_info[b"info"][b"files"]:
-                path = None
-                if isinstance(f["path"], list):
-                    path = f["path"][0].decode("UTF-8")
-                elif isinstance(f["path"], bytes):
-                    path = f["path"].decode("UTF-8")
-                self.files.append(FileItem(path, f["length"]))
 
-        else:
-            self.files.append(
-                FileItem(self.meta_info["info"]["name"].decode("UTF-8"),
-                         self.meta_info["info"]["length"]))
-
-    @property
-    def multi_file(self) -> bool:
-        """
-        TODO: proper handling of multi-file torrents.
         For a single file torrent,
             the meta_info["info"]["name"] is the torrent's
             content's file basename and meta_info["info"]["length"] is its size
@@ -249,43 +233,55 @@ class MetaInfoFile:
             meta_info["info"]["files"]["length"] is the file's size
             meta_info["info"]["length"] doesn't contribute anything here
         """
+        logger.debug(f"Gathering files for .torrent: {self}")
+
+        if self.multi_file:
+            file_list = self.meta_info["info"]["files"]
+            if not file_list:
+                logger.error("No file list.")
+                raise CreationError
+            for f in file_list:
+                length = f.get("length", 0)
+                path = Path("/".join([x.decode("UTF-8") for x in f.get("path", [])]))
+                self.files.append(FileItem(path, length))
+        else:
+            self.files.append(
+                FileItem(Path(_get_and_decode(self.meta_info["info"], "name")),
+                         self.meta_info["info"]["length"]))
+
+    @property
+    def multi_file(self) -> bool:
+        """
+        Returns True if this is a torrent with multiple files.
+        """
         return "files" in self.meta_info["info"]
 
     @property
-    def announce_urls(self) -> List[str]:
+    def announce_urls(self) -> List[List[str]]:
         """
         The announce URL of the tracker.
         According to BEP 0012 (http://bittorrent.org/beps/bep_0012.html),
         if announce-list is present, it is used instead of announce.
         :return: a list of announce URLs for the tracker
         """
-        urls: List[str] = []
+        # TODO: Properly shuffle the list (either here or in the tracker)
         if "announce-list" in self.meta_info:
-            # announce list is a list of lists of strings
-            for url_list in self.meta_info["announce-list"]:
-                inner_list: List[str] = []
-                for url in url_list:
-                    inner_list.append(url.decode("UTF-8"))
-                urls += inner_list
-        else:
-            urls.append(self.meta_info["announce"].decode("UTF-8"))
-        return urls
+            return [[x.decode("UTF-8") for x in url_list] for url_list in self.meta_info["announce-list"]]
+        return [[_get_and_decode(self.meta_info, "announce")]]
 
     @property
-    def comment(self) -> Optional[str]:
+    def comment(self) -> str:
         """
         :return: the torrent's comment
         """
-        if "comment" in self.meta_info:
-            return self.meta_info["comment"].decode("UTF-8")
+        return _get_and_decode(self.meta_info, "comment")
 
     @property
     def created_by(self) -> Optional[str]:
         """
         :return: the torrent's creation program
         """
-        if "created by" in self.meta_info:
-            return self.meta_info["created by"].decode("UTF-8")
+        return _get_and_decode(self.meta_info, "created by")
 
     @property
     def creation_date(self) -> Optional[int]:
@@ -305,7 +301,7 @@ class MetaInfoFile:
     @property
     def piece_length(self) -> int:
         """
-        :return: Length in bytes for each piece
+        :return: Nominal length in bytes for each piece
         """
         return self.meta_info["info"]["piece length"]
 
@@ -328,7 +324,7 @@ class MetaInfoFile:
         """
         :return: the total number of pieces in the torrent
         """
-        return len(self.pieces)
+        return len(self.piece_hashes)
 
     @property
     def name(self) -> str:
@@ -336,4 +332,4 @@ class MetaInfoFile:
         :return: the torrent's name; either the single filename or the directory
         name.
         """
-        return self.meta_info["info"]["name"].decode("UTF-8")
+        return _get_and_decode(self.meta_info["info"], "name")
