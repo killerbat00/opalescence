3c1a2be1cc2f57f88229b764698a75f369ed808d
brian houston morrow <killerbat00@users.noreply.github.com>
2021-03-14 22:58:53 -0500

    Wrap peer communication in an async context manager/iterator.

--- opalescence/btlib/utils.py
+++ opalescence/btlib/utils.py
@@ -1,8 +1,9 @@
+import asyncio
 import errno
 import os
 from pathlib import Path
 
-__all__ = ["ensure_dir_exists"]
+__all__ = ["ensure_dir_exists", "open_peer_connection"]
 
 
 def ensure_dir_exists(filename: Path) -> None:
@@ -19,3 +20,17 @@ def ensure_dir_exists(filename: Path) -> None:
         except OSError as exc:  # Guard against race condition
             if exc.errno != errno.EEXIST:
                 raise
+
+
+async def open_peer_connection(host=None, port=None) -> [asyncio.StreamReader, asyncio.StreamWriter]:
+    """
+    A wrapper for asyncio.open_connection() returning a (reader, writer) pair.
+    """
+    loop = asyncio.events.get_event_loop()
+    reader = asyncio.StreamReader(loop=loop)
+    protocol = asyncio.StreamReaderProtocol(reader, loop=loop)
+    transport, _ = await loop.create_connection(
+        lambda: protocol, host, port)
+    transport.set_write_buffer_limits(0)  # let the OS handle buffering
+    writer = asyncio.StreamWriter(transport, protocol, reader, loop)
+    return reader, writer
