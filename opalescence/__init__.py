523bd37edeae86dc0e447159e69104b419c16714
brian houston morrow <killerbat00@users.noreply.github.com>
2021-04-06 22:00:27 -0500

    Create temporary app config class and use it for determining appropriate output.

--- opalescence/__init__.py
+++ opalescence/__init__.py
@@ -8,3 +8,17 @@ __author__ = """Brian Houston Morrow"""
 __email__ = "bhm@brianmorrow.net"
 __version__ = "0.4.1"
 __year__ = "2021"
+
+import dataclasses
+
+
+@dataclasses.dataclass
+class AppConfig:
+    use_cli: bool
+
+
+_AppConfig = AppConfig(False)
+
+
+def get_app_config():
+    return _AppConfig
