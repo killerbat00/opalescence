5679e15499213ff366ef089a88d3eae21b7d25f4
brian houston morrow <killerbat00@users.noreply.github.com>
2021-04-08 22:17:08 -0500

    Move file related functionality to its own module.

--- opalescence/btlib/protocol/fileio.py
+++ opalescence/btlib/protocol/fileio.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
 
 """
-Handles writing completed pieces to their appropriate files on disk.
+Classes and APIs for file reading and writing.
 """
 
 from __future__ import annotations
 
 import asyncio
+import dataclasses
 import functools
 import logging
+from pathlib import Path
 from typing import Optional, BinaryIO
 
 from .messages import Piece
-from .metainfo import FileItem
 from ..utils import ensure_dir_exists
 
 logger = logging.getLogger(__name__)
@@ -22,33 +23,42 @@ class FileWriter:
 
     def __init__(self, files: dict[int, FileItem]):
         self._files: dict[int, FileItem] = files
-        self._total_size = sum([file.size for file in self._files.values()])
         self._lock = asyncio.Lock()
         self._fps: Optional[dict[int, BinaryIO]] = None
 
-    def __enter__(self):
+    def write_piece(self, piece):
         """
-        Facilitates use as a context manager, opening file streams if necessary.
-        """
-        if self._fps is None:
-            self._open_files(self._files)
-        return self
+        Schedules a task that will write a given piece to the appropriate
+        file in an executor that will not block the main thread.
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+        :param piece: piece to be written
         """
-        Exits the context manager, closing files if possible.
-        Propagates any exception that caused this context to be exited.
+        asyncio.ensure_future(self.__await_write(piece))
+
+    def close(self):
         """
+        Closes all open file streams.
+        """
+        self.__close_files()
+
+    def __open_files(self):
+        """
+        Opens/creates all the files that will be written, storing the open streams.
+        """
+        if self._files is None or len(self._files) == 0:
+            return
+
+        file = ""
+        self._fps = {}
         try:
-            self._close_files()
-
-            if exc_type is None and exc_val is None and exc_tb is None:
-                return True
+            for i, file in self._files.items():
+                ensure_dir_exists(file.path)
+                self._fps[i] = open(file.path, "wb+")
         except Exception:
-            pass
-        return False
+            logger.error("Encountered exception opening %s" % file.path)
+            raise
 
-    def _close_files(self):
+    def __close_files(self):
         """
         Closes all open file streams.
         """
@@ -59,57 +69,27 @@ class FileWriter:
             if not fp.closed:
                 fp.close()
 
-    def _open_files(self, files: dict[int, FileItem]):
-        """
-        Opens/creates all the files that will be written, storing the open streams.
-        :param files: dictionary of `FileItem`s this `FileWriter` is writing.
-        """
-        file = ""
-        self._fps = {}
-        try:
-            for i, file in files.items():
-                ensure_dir_exists(file.path)
-                self._fps[i] = open(file.path, "wb+")
-        except Exception:
-            logger.error(f"Encountered exception opening {file.path}")
-            raise
-
-    def _write_data(self, data_to_write, file_num, offset):
-        """
-        Writes data to the file in an executor so we don't block the main thread.
-        :param data_to_write: data to write to file
-        :param file_num: file index in self._fps to write to
-        :param offset: Offset into the file to begin writing this data
-        """
-        fp = self._fps[file_num]
-        try:
-            if fp.closed:
-                raise OSError
-
-            fp.seek(offset, 0)
-            fp.write(data_to_write)
-        except (OSError, Exception):
-            logger.error(f"Encountered exception when writing to {fp.name}")
-            raise
-
-    async def write(self, piece: Piece):
+    async def __await_write(self, piece: Piece):
         """
         Writes the `Piece` to the file in an executor. Pieces can be written in any order.
         :param piece: piece to write.
         """
+        if self._fps is None:
+            self.__open_files()
+
         await self._lock.acquire()
 
         try:
             await asyncio.get_running_loop().run_in_executor(None,
-                                                             functools.partial(self._write, piece))
-            piece.mark_complete()
+                                                             functools.partial(self.__write_piece_data, piece))
+            piece.mark_complete()  # purge from memory
         except Exception as e:
             logger.exception(e)
             raise
         finally:
             self._lock.release()
 
-    def _write(self, piece: Piece):
+    def __write_piece_data(self, piece: Piece):
         """
         Writes the piece's data to the appropriate file(s). Pieces can be written in any order.
         :param piece: piece to write.
@@ -125,7 +105,7 @@ class FileWriter:
                 logger.error("Too much data and not enough files...")
                 raise
 
-            logger.info(f"Writing data to {file.path}")
+            logger.info(f"Writing data to %s" % file.path)
 
             if file_offset + len(data_to_write) > file.size:
                 data_for_file = data_to_write[:file.size - file_offset]
@@ -134,4 +114,22 @@ class FileWriter:
             else:
                 data_for_file = data_to_write
                 data_to_write = None
-            self._write_data(data_for_file, file_num, file_offset)
+            self.__write_data(data_for_file, file_num, file_offset)
+
+    def __write_data(self, data_to_write, file_num, offset):
+        """
+        Writes data to the file in an executor so we don't block the main thread.
+        :param data_to_write: data to write to file
+        :param file_num: file index in self._fps to write to
+        :param offset: Offset into the file to begin writing this data
+        """
+        fp = self._fps[file_num]
+        try:
+            if fp.closed:
+                raise OSError
+
+            fp.seek(offset, 0)
+            fp.write(data_to_write)
+        except (OSError, Exception):
+            logger.error("Encountered exception when writing to %s" % fp.name)
+            raise
