0fb6aea234f39f86c9c7248502faa9cd513ce8bc
brian houston morrow <killerbat00@users.noreply.github.com>
2021-05-08 00:01:01 -0500

    Begin structuring the TUI piecemeal.

--- opalescence/ui/tui.py
+++ opalescence/ui/tui.py
@@ -5,6 +5,7 @@ Terminal User Interface for Opalescence
 """
 import curses
 import curses.ascii
+import functools
 import logging
 import time
 
@@ -15,6 +16,9 @@ logger = logging.getLogger("opalescence")
 PROG_NAME = f"Opalescence v{__version__}"
 WELCOME_MSG = f"Welcome to {PROG_NAME}"
 SIG_MSG = f"{PROG_NAME} was created by: {__author__} (c) {__year__}"
+
+COLOR_NONE = None
+COLOR_INVERTED = None
 
 
 class WelcomeSplash:
@@ -45,37 +49,77 @@ class HeaderLine:
         window.addnstr(0, 0, PROG_NAME.center(width, " "), width, clr)
 
 
+class FooterLine:
+    TORRENT_COMMANDS = {
+        "Add": ("(A)dd", lambda x: x),
+        "Delete": ("(D)elete", lambda x: x),
+        "Files": ("(F)iles", lambda x: x),
+        "Config": ("(C)onfig", lambda x: x),
+    }
+
+    APP_COMMANDS = {
+        "Settings": ("(S)ettings", lambda x: x),
+        "Quit": ("(Q)uit", lambda x: x),
+        # "QuitWindow": ("(Q)uit*", lambda x: x)
+    }
+
+    @staticmethod
+    def draw(window, clr):
+        width = curses.COLS
+        pos_y = curses.LINES - 1
+        left = " | ".join(cmd[0] for cmd in FooterLine.TORRENT_COMMANDS.values())
+        window.addnstr(pos_y, 0, left, len(left), clr)
+        right = " | ".join(cmd[0] for cmd in FooterLine.APP_COMMANDS.values())
+
+        try:
+            window.addnstr(pos_y, width - len(right), right, len(right) + 1, clr)
+        except curses.error:
+            pass
+
+
 class MainScreen:
     @staticmethod
     def draw(window, clr_none, clr_inv):
         window.clear()
         window.border()
         HeaderLine.draw(window, clr_inv)
+        FooterLine.draw(window, clr_inv)
         window.refresh()
 
 
-COLOR_NONE = None
-COLOR_INVERTED = None
+class Command:
+    def __init__(self, name, func):
+        self.name = name
+        self.func = func
 
 
-def main(root):
-    global COLOR_NONE, COLOR_INVERTED
-    curses.curs_set(0)
-    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE)
-    COLOR_NONE = curses.color_pair(1)
-    COLOR_INVERTED = curses.color_pair(2)
-    root.clear()
-    WelcomeSplash.draw(root, COLOR_INVERTED)
-    MainScreen.draw(root, COLOR_NONE, COLOR_INVERTED)
-    curses.flushinp()
-    root.getch()
-    root.clear()
-    curses.curs_set(1)
+class OplTui:
+    def __init__(self):
+        self.color_none = None
+        self.color_inverted = None
+        # self.cmds: list[Command] = []
+        self.root_win = None
+
+    def main(self, root):
+        self.root_win = root
+
+        curses.curs_set(0)
+        curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
+        curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE)
+        self.color_none = curses.color_pair(1)
+        self.color_inverted = curses.color_pair(2)
+
+        self.root_win.clear()
+        WelcomeSplash.draw(self.root_win, self.color_inverted)
+        MainScreen.draw(self.root_win, self.color_none, self.color_inverted)
+        curses.flushinp()
+        self.root_win.getch()
+        curses.curs_set(1)
 
 
 def start(args):
     """
     TUI entry point
     """
-    curses.wrapper(main)
+    tui = OplTui()
+    curses.wrapper(functools.partial(tui.main))
